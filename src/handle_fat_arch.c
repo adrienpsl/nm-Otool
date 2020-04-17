678a66c46edaa68ad8d6a0f552818b91d7571b33
adpusel <adrien.pusel@gmail.com>
2020-04-17 19:02:36 +0200

    ok, with ok binary I do the job!

--- src/handle_fat_arch.c
+++ src/handle_fat_arch.c
@@ -12,10 +12,8 @@
 
 #include "nm_otool.h"
 
-void print_fat_header(t_no *no, struct fat_arch *fat_arch)
+void print_fat_header(struct fat_arch *fat_arch)
 {
-	if (no->fat_start == NULL)
-		no->fat_start = no->start_map + swapif32(fat_arch->offset);
 	if (1)
 		return;
 	ft_printf("    Cpu type:    %d\n", swapif32(fat_arch->cputype));
@@ -29,18 +27,44 @@ void handle_fat_binaries(t_no *no)
 {
 	uint32_t arch_nb;
 	uint32_t i;
+	struct fat_arch *fat_arch;
 
 	if (no->is_fat == false)
 		return;
 	arch_nb = swapif32(((struct fat_header *)no->map)->nfat_arch);
 	i = 0;
-	no->map += sizeof(struct fat_header);
+	fat_arch = no->map + sizeof(struct fat_header);
 	while (i < arch_nb)
 	{
-		//		ft_printf("architecture %u\n", i);
-		print_fat_header(no, no->map);
-		no->map += sizeof(struct fat_arch);
+		if (swapif32(fat_arch->cputype) == 16777223)
+		{
+			no->fat_start = no->start_map + swapif32(fat_arch->offset);
+		}
+		if (no->fat_start == NULL)
+			no->fat_start = no->start_map + swapif32(fat_arch->offset);
+		print_fat_header(no->map);
+		fat_arch = (void *)fat_arch + sizeof(struct fat_arch);
 		i++;
 	}
 	no->map = no->fat_start;
 }
+
+//void handle_fat_binaries(t_no *no)
+//{
+//	uint32_t arch_nb;
+//	uint32_t i;
+//
+//	if (no->is_fat == false)
+//		return;
+//	arch_nb = swapif32(((struct fat_header *)no->map)->nfat_arch);
+//	i = 0;
+//	no->map += sizeof(struct fat_header);
+//	while (i < arch_nb)
+//	{
+//		//		ft_printf("architecture %u\n", i);
+//		print_fat_header(no, no->map);
+//		no->map += sizeof(struct fat_arch);
+//		i++;
+//	}
+//	no->map = no->fat_start;
+//}
