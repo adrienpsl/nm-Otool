9a89b236ac7c1d36ef46c2fa5969533314c7d865
adpusel <adrien.pusel@gmail.com>
2020-04-22 14:29:00 +0200

    I pass all test without option with nm

--- src/sym_array.print.c
+++ src/sym_array.print.c
@@ -37,12 +37,14 @@ void print_sym(t_ofile *ofile, struct nlist_64 *symbol)
 	symbol_name = get_name(symbol);
 	type = get_symbol_type(ofile->sections, symbol);
 	if (get_options()->u_only_undef && type == 'U')
-		ft_putstr_lim(symbol_name);
-	else if (type != 'U')
-		print_local(symbol, symbol_name, type);
-	else
-		is_64bits() ? ft_printf("%18c %s\n", type, symbol_name)
-					: ft_printf("%10c %s\n", type, symbol_name);
+		return (ft_putstr_lim(symbol_name));
+	else if (type != 'U' && type != 'I')
+		return (print_local(symbol, symbol_name, type));
+	is_64bits() ? ft_printf("%18c %s", type, symbol_name)
+				: ft_printf("%10c %s", type, symbol_name);
+	if(type == 'I')
+		ft_printf(" (indirect for %s)", symbol_name);
+	ft_putchar('\n');
 }
 
 void print_sym_array(t_ofile *file)
--- test_script.sh
+++ test_script.sh
@@ -13,10 +13,9 @@ for file in /usr/bin/*; do
   file_res=$(echo "$file" | sed -e "s/^\/usr\/bin\///")
   [ ! -z "$res" ] &&  echo "$res" > "test/$file_res"
 done
-
-
+#
+#
 for file in ./binaire_test/*; do
-  echo "$file"
   nm "$file" > real_nm 2>&1
 #  nm  "$file" > real_nm 2>&1
   ./cmake-build-debug/nm_otool "$file" > my_nm 2>&1
@@ -24,9 +23,8 @@ for file in ./binaire_test/*; do
   file_res=$(echo "$file" | sed -e "s/^\.\/binaire_test\///")
   [ ! -z "$res" ] &&  echo "$res" > "test/$file_res"
 done
-
+#
 for file in /bin/*; do
-  echo "$file"
   nm "$file" > real_nm 2>&1
 #  nm  "$file" > real_nm 2>&1
   ./cmake-build-debug/nm_otool "$file" > my_nm 2>&1
@@ -35,17 +33,24 @@ for file in /bin/*; do
   [ ! -z "$res" ] &&  echo "$res" > "test/$file_res"
 done
 
+for file in /usr/lib/system/*; do
+  nm "$file" | cat -e > real_nm 2>&1
+#  nm  "$file" > real_nm 2>&1
+  ./cmake-build-debug/nm_otool "$file" | cat -e > my_nm 2>&1
+  res=$(diff real_nm my_nm)
+  file_res=$(echo "$file" | sed -e "s/^\/usr\/lib\/system\///")
+  [ ! -z "$res" ] &&  echo "$res" > "test/$file_res"
+done
 
 
-#for file in ./binaire_test/*; do
-#  echo "$file"
-#  nm "$file" > real_nm 2>&1
-##  nm  "$file" > real_nm 2>&1
-#  ./cmake-build-debug/nm_otool "$file" > my_nm 2>&1
-#  res=$(diff real_nm my_nm)
-#  file_res=$(echo "$file" | sed -e "s/^\.\/binaire_test\///")
-#  [ ! -z "$res" ] &&  echo "$res" > "test/$file_res"
-#done
+for file in ./objs/*; do
+  nm "$file" > real_nm 2>&1
+#  nm  "$file" > real_nm 2>&1
+  ./cmake-build-debug/nm_otool "$file" > my_nm 2>&1
+  res=$(diff real_nm my_nm)
+  file_res=$(echo "$file" | sed -e "s/^\.\/objs\///")
+  [ ! -z "$res" ] &&  echo "$res" > "test/$file_res"
+done
 
 cd test
 ls -l | wc -l
